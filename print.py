bea4fd6291b2977f062b323863cc87ec606c0816
Lanim <semeboerch@gmail.com>
2023-09-10 12:41:55 -0500

    come

--- print.py
+++ print.py
@@ -116,6 +116,6 @@ print(name)
 pan = os.getenv("pan", default="shan")
 print(pan)
 
-with open("/tmp/2.txt", "w") as file:
+with open("/home/2.txt", "w") as file:
     file.write("example.\n")
     file.write("It was created using the OS module in python.")
