76ac085f23d781f0c5d36ff042e56fbea10f8ce0
lanim234 <niyi_meg@yahoo.com>
2023-10-04 14:15:04 -0500

    h

--- party.py
+++ party.py
@@ -16,4 +16,4 @@ print(exit_status)
 
 with open ('/tmp/1.txt','w') as file:
     file.write('This is a sample file./n')
-    file.write('This was created using the OS module in python')
+    file.write('This was created using the OS module in python craft')
